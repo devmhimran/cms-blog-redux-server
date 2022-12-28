2050f11b729a9bc50abb3f1dbdfe48816f5335c0
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-12-28 09:38:27 +0600

    Added Required Files

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+var cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fshf8lh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+  
+      const categoryCollection = client.db('cms-blog-redux').collection('category');
+  
+      app.get('/category', async (req,res) =>{
+        const query = {};
+        const cursor = categoryCollection.find(query);
+        const data = await cursor.toArray();
+        res.send(data);
+      });
+  
+  
+      
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
