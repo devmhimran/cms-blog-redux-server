3672e4bf2da19992ab7700c3c533a3bd9154905c
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-12-29 07:59:57 +0600

    Added Get Category & Add Category

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 var cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -16,25 +16,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try {
-  
-      const categoryCollection = client.db('cms-blog-redux').collection('category');
-  
-      app.get('/category', async (req,res) =>{
-        const query = {};
-        const cursor = categoryCollection.find(query);
-        const data = await cursor.toArray();
-        res.send(data);
-      });
-  
-  
-      
-    } finally {
-      // await client.close();
-    }
+  try {
+
+    const categoryCollection = client.db('cms-blog-redux').collection('category');
+
+    app.get('/category', async (req, res) => {
+      const query = {};
+      const cursor = categoryCollection.find(query);
+      const data = await cursor.toArray();
+      res.send(data);
+    });
+
+    app.post('/category-upload', (req, res) => {
+      const addData = req.body;
+      const result = categoryCollection.insertOne(addData)
+      res.send(result)
+    })
+
+    app.delete('/category-delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await categoryCollection.deleteOne({_id: ObjectId(id)})
+      res.send(result)
+    })
+
+
+  } finally {
+    // await client.close();
   }
-  run().catch(console.dir);
-  
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
